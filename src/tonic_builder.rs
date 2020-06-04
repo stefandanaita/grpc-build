dce89a18f0b1ff20155fd6df2e4f72162fccf36a
Luca Palmieri <lpalmieri@truelayer.com>
2020-06-04 21:41:54 +0100

    Clean paths

--- src/tonic_builder.rs
+++ src/tonic_builder.rs
@@ -1,5 +1,6 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::{fs, io};
+use std::ffi::OsStr;
 
 pub fn compile(
     input_dir: &str,
@@ -7,7 +8,7 @@ pub fn compile(
     server: bool,
     client: bool,
 ) -> Result<(), anyhow::Error> {
-    let mut protos: Vec<String> = vec![];
+    let mut protos = vec![];
     get_protos(protos.as_mut(), Path::new(input_dir))?;
 
     fs::create_dir_all(output_dir)?;
@@ -15,12 +16,12 @@ pub fn compile(
         .out_dir(output_dir)
         .build_client(server)
         .build_server(client)
-        .compile(protos.as_slice(), &[String::from(".")])?;
+        .compile(protos.as_slice(), &[PathBuf::from(".")])?;
 
     Ok(())
 }
 
-fn get_protos(protos: &mut Vec<String>, dir: &Path) -> io::Result<()> {
+fn get_protos(protos: &mut Vec<PathBuf>, dir: &Path) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
@@ -29,14 +30,9 @@ fn get_protos(protos: &mut Vec<String>, dir: &Path) -> io::Result<()> {
             if path.is_dir() {
                 get_protos(protos, &path)?;
             } else {
-                let os_ext = &path.extension();
-
-                if os_ext.is_some() {
-                    let ext = os_ext.unwrap().to_str().unwrap();
-
-                    if ext.contains("proto") {
-                        let path_str = path.as_path().to_str().unwrap();
-                        protos.push(String::from(path_str));
+                if let Some(extension) = path.extension() {
+                    if extension == OsStr::new("proto") {
+                        protos.push(path.as_path().to_owned());
                     }
                 }
             }
