878d343d3a159e38b409ecc18b3b6d3a46d6d793
Stefan Adrian Danaita <me@dsa.io>
2020-06-04 18:51:27 +0000

    CLI tool wip working

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,48 @@ use std::fs::File;
 mod graph_layout;
 mod tonic_builder;
 
+#[derive(structopt::StructOpt)]
+pub enum Command {
+    Build {
+        in_dir: Option<String>,
+        out_dir: Option<String>,
+
+        #[structopt(short = "client", long = "build_client")]
+        build_client: bool,
+
+        #[structopt(short = "server", long = "build_server")]
+        build_server: bool,
+    },
+}
+
 #[async_std::main]
 async fn main() -> Result<(), anyhow::Error> {
-    compile("protos", "compiled", true, true)?;
+    let command = <Command as paw::ParseArgs>::parse_args()?;
 
-    let graph = generate("compiled")?;
+    match command {
+        Command::Build {
+            in_dir,
+            out_dir,
+            build_client,
+            build_server,
+        } => {
+            let input_dir: String = match in_dir {
+                None => String::from("protos"),
+                Some(dir) => dir,
+            };
+            let output_dir: String = match out_dir {
+                None => String::from("compiled"),
+                Some(dir) => dir,
+            };
 
-    let mut proto_lib = File::create("src/lib.rs")?;
-    display(&graph, &mut proto_lib, &NodeIndex::from(0))?;
+            compile(&input_dir, &output_dir, build_server, build_client)?;
+
+            let graph = generate(&output_dir)?;
+
+            let mut proto_lib = File::create("src/lib.rs")?;
+            display(&graph, &mut proto_lib, &NodeIndex::from(0))?;
+        }
+    }
 
     Ok(())
 }
