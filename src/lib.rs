a1f2102f51ae388e23b3f057e539458208fa8d64
Stefan Adrian Danaita <me@dsa.io>
2020-06-08 20:43:25 +0000

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,29 @@ use crate::graph_layout::{display, generate};
 use crate::tonic_builder::compile;
 use petgraph::graph::NodeIndex;
 use std::fs::File;
+use std::fs;
+use std::path::Path;
+use thiserror::Error;
 
 mod graph_layout;
 mod tonic_builder;
+
+#[derive(Error, Debug)]
+pub enum BuildError {
+    #[error("The output directory already exists: {0}")]
+    OutputDirectoryExistsError(String),
+
+    #[error("Generic error")]
+    GenericError(),
+}
 
 pub fn build(
     in_dir: Option<String>,
     out_dir: Option<String>,
     build_server: bool,
     build_client: bool,
-) -> Result<(), anyhow::Error> {
+    force: bool,
+) -> Result<(), BuildError> {
     let input_dir: String = match in_dir {
         None => String::from("protos"),
         Some(dir) => dir,
@@ -21,12 +34,41 @@ pub fn build(
         Some(dir) => dir,
     };
 
-    compile(&input_dir, &output_dir, build_server, build_client)?;
+    if Path::new(&output_dir).exists() {
+        if !force {
+            return Err(BuildError::OutputDirectoryExistsError(String::from(&output_dir)));
+        }
 
-    let graph = generate(&output_dir)?;
+        match fs::remove_dir_all(&output_dir) {
+            Ok(_) => {},
+            Err(_) => return Err(BuildError::GenericError()),
+        };
+    }
 
-    let mut proto_lib = File::create(format!("{}/mod.rs", output_dir))?;
-    display(&graph, &mut proto_lib, &NodeIndex::from(0))?;
+    match fs::create_dir_all(&output_dir) {
+        Ok(_) => {},
+        Err(_) => return Err(BuildError::GenericError()),
+    };
+
+    match compile(&input_dir, &output_dir, build_server, build_client) {
+        Ok(_) => {},
+        Err(_) => return Err(BuildError::GenericError()),
+    };
+
+    let graph = match generate(&output_dir) {
+        Ok(graph) => graph,
+        Err(_) => return Err(BuildError::GenericError()),
+    };
+
+    let mut proto_lib = match File::create(format!("{}/mod.rs", output_dir)) {
+        Ok(file) => file,
+        Err(_) => return Err(BuildError::GenericError()),
+    };
+
+    match display(&graph, &mut proto_lib, &NodeIndex::from(0)) {
+        Ok(_) => {},
+        Err(_) => return Err(BuildError::GenericError()),
+    };
 
     Ok(())
 }
