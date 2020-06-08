270b9d2ea041d3d6bc9b8255021d2f4ea9c7ed86
Stefan Adrian Danaita <me@dsa.io>
2020-06-08 22:23:33 +0000

    More meaningful errors

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,8 @@ pub enum BuildError {
     #[error("The output directory already exists: {0}")]
     OutputDirectoryExistsError(String),
 
-    #[error("Generic error")]
-    GenericError(),
+    #[error("{0}")]
+    Error(String),
 }
 
 pub fn build(
@@ -41,33 +41,33 @@ pub fn build(
 
         match fs::remove_dir_all(&output_dir) {
             Ok(_) => {},
-            Err(_) => return Err(BuildError::GenericError()),
+            Err(_) => return Err(BuildError::Error(String::from("Could not remove the output directory"))),
         };
     }
 
     match fs::create_dir_all(&output_dir) {
         Ok(_) => {},
-        Err(_) => return Err(BuildError::GenericError()),
+        Err(_) => return Err(BuildError::Error(String::from("Could not create the output directory"))),
     };
 
     match compile(&input_dir, &output_dir, build_server, build_client) {
         Ok(_) => {},
-        Err(_) => return Err(BuildError::GenericError()),
+        Err(_) => return Err(BuildError::Error(String::from("Failed the compile the protos"))),
     };
 
     let graph = match generate(&output_dir) {
         Ok(graph) => graph,
-        Err(_) => return Err(BuildError::GenericError()),
+        Err(_) => return Err(BuildError::Error(String::from("Failed to generate the graph"))),
     };
 
     let mut proto_lib = match File::create(format!("{}/mod.rs", output_dir)) {
         Ok(file) => file,
-        Err(_) => return Err(BuildError::GenericError()),
+        Err(_) => return Err(BuildError::Error(String::from("Failed to create the mod.rs file"))),
     };
 
     match display(&graph, &mut proto_lib, &NodeIndex::from(0)) {
         Ok(_) => {},
-        Err(_) => return Err(BuildError::GenericError()),
+        Err(_) => return Err(BuildError::Error(String::from("Failed to populate the mod.rs file"))),
     };
 
     Ok(())
