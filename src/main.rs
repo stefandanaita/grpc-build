422651bd9d8422d46cd762231c75428fee2b22ff
Stefan Adrian Danaita <me@dsa.io>
2020-06-08 16:57:58 +0000

    Use mod.rs instead of lib.rs

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ pub enum Command {
     Build {
         #[structopt(long)]
         in_dir: Option<String>,
+
+        #[structopt(long)]
         out_dir: Option<String>,
 
         #[structopt(short = "client", long = "build_client")]
@@ -37,7 +39,7 @@ async fn main() -> Result<(), anyhow::Error> {
                 Some(dir) => dir,
             };
             let output_dir: String = match out_dir {
-                None => String::from("compiled"),
+                None => String::from("src/protogen"),
                 Some(dir) => dir,
             };
 
@@ -45,7 +47,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
             let graph = generate(&output_dir)?;
 
-            let mut proto_lib = File::create("src/lib.rs")?;
+            let mut proto_lib = File::create(format!("{}/mod.rs", output_dir))?;
             display(&graph, &mut proto_lib, &NodeIndex::from(0))?;
         }
     }
