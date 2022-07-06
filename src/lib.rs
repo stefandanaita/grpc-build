07dd0a656b5c1ad90817379a0d3bd89039a5d5f6
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-07-06 09:53:58 +0100

    Revert "feat: build with prost"
    This reverts commit 2d1a06000969c727e51cd11083131c17dcbc3156.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 use anyhow::{anyhow, Context, Result};
 use prost::Message;
-use prost_build::{protoc, protoc_include, Config, ServiceGenerator};
 use prost_types::{FileDescriptorProto, FileDescriptorSet};
 use std::{
     collections::HashMap,
-    ffi::OsStr,
     path::{Path, PathBuf},
-    process::Command,
 };
 use tonic_build::Builder;
 
@@ -46,61 +43,12 @@ pub fn build_with_config(
     Ok(())
 }
 
-pub fn build_with_prost(
-    in_dir: &str,
-    out_dir: &str,
-    build_server: bool,
-    build_client: bool,
-    force: bool,
-) -> Result<()> {
-    build_with_prost_config(
-        in_dir,
-        out_dir,
-        force,
-        &[] as &[&str],
-        |c| c,
-        |c: Builder| {
-            c.build_client(build_client)
-                .build_server(build_server)
-                .service_generator()
-        },
-    )
-}
-
-pub fn build_with_prost_config(
-    in_dir: &str,
-    out_dir: &str,
-    force: bool,
-    protoc_args: &[impl AsRef<OsStr>],
-    prost_config: impl FnOnce(Config) -> Config,
-    service_generator: impl FnOnce(Builder) -> Box<dyn ServiceGenerator>,
-) -> Result<()> {
-    if !force && Path::new(out_dir).exists() {
-        return Err(anyhow!("the output directory already exists: {}", out_dir));
-    }
-
-    base::prepare_out_dir(out_dir).context("failed to prepare out dir")?;
-
-    prost_compile(
-        in_dir,
-        out_dir,
-        protoc_args,
-        prost_config,
-        service_generator,
-    )
-    .context("failed to compile the protos")?;
-
-    base::refactor(out_dir).context("failed to refactor the protos")?;
-
-    Ok(())
-}
-
-fn prost_compile(
+fn compile(
     input_dir: &str,
     output_dir: &str,
-    protoc_args: &[impl AsRef<OsStr>],
-    prost_config: impl FnOnce(Config) -> Config,
-    service_generator: impl FnOnce(Builder) -> Box<dyn ServiceGenerator>,
+    server: bool,
+    client: bool,
+    user_config: impl Fn(Builder) -> Builder,
 ) -> Result<(), anyhow::Error> {
     let protos = crate::base::get_protos(input_dir).collect::<Vec<_>>();
 
@@ -109,35 +57,28 @@ fn prost_compile(
         Some(parent) => parent.to_path_buf(),
     };
 
+    // Steps:
+    // 1. compile protos along with the filedescriptor set in a temp folder
+    // 2. then we parse the filedescriptor set to obtain all message names
+    // 3. add the proc macro annotation to generate the `full_proto_name` function.
+    // 4. finally compile the protos using the annotation in the user-defined dir
+
     let tmp = tempfile::Builder::new().prefix("grpc-build").tempdir()?;
     let file_descriptor_path = tmp.path().join("grpc-descriptor-set");
+    let tmp_dir = tempfile::Builder::new().prefix("grpc-build").tempdir()?;
 
-    let mut cmd = Command::new(protoc());
-    cmd.arg("--include_imports")
-        .arg("--include_source_info")
-        .arg("-o")
-        .arg(&file_descriptor_path);
-
-    cmd.arg("-I").arg(&compile_includes);
-
-    // Set the protoc include after the user includes in case the user wants to
-    // override one of the built-in .protos.
-    cmd.arg("-I").arg(protoc_include());
-
-    for arg in protoc_args {
-        cmd.arg(arg);
-    }
-
-    for proto in &protos {
-        cmd.arg(proto);
-    }
-
-    let _output = cmd.output().map_err(|error| {
-        anyhow::anyhow!(format!(
-            "failed to invoke protoc (hint: https://docs.rs/prost-build/#sourcing-protoc): {}",
-            error
-        ),)
-    })?;
+    user_config(
+        tonic_build::configure()
+            .build_client(client)
+            .build_server(server)
+            // HACK: we compile it once to get the file_descriptor_set.
+            // But we just hide it in a temp folder.
+            // This is because tonci_build does not expose the `skip_protoc_run` fucntion
+            // like prost_build so we cannot skip it.
+            .out_dir(tmp_dir.path())
+            .file_descriptor_set_path(&file_descriptor_path),
+    )
+    .compile(&protos, &[&compile_includes])?;
 
     let buf = std::fs::read(&file_descriptor_path)?;
     let file_descriptor_set =
@@ -153,43 +94,18 @@ fn prost_compile(
                 acc
             });
 
-    let mut config = prost_config(prost_build::Config::new());
+    let mut config = tonic_build::configure();
     for (name, annotation) in &annotations {
-        config.type_attribute(&name, annotation);
+        config = config.type_attribute(&name, annotation);
     }
 
-    config
-        .skip_protoc_run()
-        .out_dir(output_dir)
-        .file_descriptor_set_path(&file_descriptor_path)
-        .service_generator(service_generator(tonic_build::configure()));
-
-    config
-        .compile_protos(&protos, &[&compile_includes])
-        .context("failed to generate rust files")?;
-
-    Ok(())
-}
-
-fn compile(
-    input_dir: &str,
-    output_dir: &str,
-    server: bool,
-    client: bool,
-    user_config: impl Fn(Builder) -> Builder,
-) -> Result<(), anyhow::Error> {
-    let protos = crate::base::get_protos(input_dir).collect::<Vec<_>>();
-
-    let compile_includes: PathBuf = match Path::new(input_dir).parent() {
-        None => PathBuf::from("."),
-        Some(parent) => parent.to_path_buf(),
-    };
-
-    user_config(tonic_build::configure())
-        .build_client(client)
-        .build_server(server)
-        .out_dir(output_dir)
-        .compile(&protos, &[&compile_includes])?;
+    user_config(
+        config
+            .build_client(client)
+            .build_server(server)
+            .out_dir(output_dir),
+    )
+    .compile(&protos, &[&compile_includes])?;
 
     Ok(())
 }
--- tests/main.rs
+++ tests/main.rs
@@ -12,18 +12,3 @@ fn build() {
     let t = trybuild::TestCases::new();
     t.pass("tests/compile_test/definitions_exist.rs");
 }
-
-#[test]
-fn build_with_prost() {
-    grpc_build::build_with_prost(
-        "tests/protos/grpc_build",
-        "tests/compile_test/protos",
-        true,
-        true,
-        true,
-    )
-    .unwrap();
-
-    let t = trybuild::TestCases::new();
-    t.pass("tests/compile_test/fully_qualified_name.rs");
-}
