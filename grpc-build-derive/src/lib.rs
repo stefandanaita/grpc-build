43865e1367fa546a710083b40bcd7da841c9bdbd
Sede Soukossi <4968379+styvane@users.noreply.github.com>
2025-04-12 16:46:09 +0200

    chore: bump syn version to v2

--- grpc-build-derive/src/lib.rs
+++ grpc-build-derive/src/lib.rs
@@ -2,7 +2,7 @@
 //! returning the full name of the message including the package namespace.
 
 use proc_macro::TokenStream;
-use syn::{spanned::Spanned, DeriveInput, Lit, MetaNameValue};
+use syn::{spanned::Spanned, DeriveInput, MetaNameValue};
 
 #[proc_macro_derive(NamedMessage, attributes(name))]
 pub fn fully_qualified_name(input: TokenStream) -> TokenStream {
@@ -23,11 +23,11 @@ fn impl_fully_qualified_name(ast: &syn::DeriveInput) -> syn::Result<TokenStream>
     };
 
     // search for #[name]
-    let mut name_attrs = ast.attrs.iter().filter(|attr| attr.path.is_ident("name"));
+    let mut name_attrs = ast.attrs.iter().filter(|attr| attr.path().is_ident("name"));
 
     // Let's assume we only have one annotation
     let meta = match name_attrs.next() {
-        Some(attr) => attr.parse_meta()?,
+        Some(attr) => &attr.meta,
         None => return Err(syn::Error::new(ast.span(), "missing #[name] attribute")),
     };
 
@@ -35,11 +35,18 @@ fn impl_fully_qualified_name(ast: &syn::DeriveInput) -> syn::Result<TokenStream>
     //   path    Lit
     let message_name = match meta {
         syn::Meta::NameValue(MetaNameValue {
-            lit: Lit::Str(name),
+            value:
+                syn::Expr::Lit(syn::ExprLit {
+                    lit: syn::Lit::Str(name),
+                    ..
+                }),
             ..
         }) => name,
-        syn::Meta::NameValue(MetaNameValue { lit, .. }) => {
-            return Err(syn::Error::new(lit.span(), "message name MUST be a string"))
+        syn::Meta::NameValue(MetaNameValue { value, .. }) => {
+            return Err(syn::Error::new(
+                value.span(),
+                "message name MUST be a string",
+            ))
         }
         meta => return Err(syn::Error::new(meta.span(), "missing #[name] attribute")),
     };
